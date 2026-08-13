987b57befab8247a1dfc58e90653fde249de92b9
Stefan Kuhn <stefan.kuhn.zurich@gmail.com>
2026-08-13 02:16:19 +0200

    Split distro installer pre and post steps

--- scripts/os-installers/distro/arch/install.sh
+++ scripts/os-installers/distro/arch/install.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-pacman -Syu --noconfirm
-
 script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 install_dir="${script_dir}/install"
+
+"${script_dir}/pre-install.sh"
 
 for install_script in "${install_dir}"/*.sh; do
   "${install_script}"
 done
 
-rm -rf /var/cache/pacman/pkg/* /var/lib/pacman/sync/*
+"${script_dir}/post-install.sh"
--- scripts/os-installers/distro/redhat/install.sh
+++ scripts/os-installers/distro/redhat/install.sh
@@ -5,12 +5,12 @@ script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 # shellcheck source=scripts/os-installers/distro/redhat/helpers.sh
 . "${script_dir}/helpers.sh"
 
-dnf -y makecache
-
 install_dir="${script_dir}/install"
+
+"${script_dir}/pre-install.sh"
 
 for install_script in "${install_dir}"/*.sh; do
   "${install_script}"
 done
 
-dnf clean all
+"${script_dir}/post-install.sh"

6f05c14619b852b4338abe82aab4f04119e772f4
Stefan Kuhn <stefan.kuhn.zurich@gmail.com>
2026-08-13 02:26:34 +0200

    Run distro pre and post installers from Dockerfile

--- scripts/os-installers/distro/alpine/install.sh
+++ scripts/os-installers/distro/alpine/install.sh
@@ -1,15 +1,8 @@
 #!/bin/sh
 set -eu
 
-apk add --no-cache "${AICAGE_PACKAGE_BASH_INSTALL:-bash}"
-
 script_dir="$(CDPATH='' cd -- "$(dirname "$0")" && pwd)"
 install_dir="${script_dir}/install"
-
-"${script_dir}/pre-install.sh"
-
 for install_script in "${install_dir}"/*.sh; do
   "${install_script}"
 done
-
-"${script_dir}/post-install.sh"

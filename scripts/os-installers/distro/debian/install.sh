aae5068d2e61f96d3b742eeb34ca503f534f5eab
Stefan Kuhn <stefan.kuhn.zurich@gmail.com>
2026-08-13 02:16:19 +0200

    Split distro installer pre and post steps

--- scripts/os-installers/distro/debian/install.sh
+++ scripts/os-installers/distro/debian/install.sh
@@ -1,14 +1,13 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-apt-get update
-
 script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 install_dir="${script_dir}/install"
+
+"${script_dir}/pre-install.sh"
 
 for install_script in "${install_dir}"/*.sh; do
   "${install_script}"
 done
 
-apt-get clean
-rm -rf /var/lib/apt/lists/*
+"${script_dir}/post-install.sh"

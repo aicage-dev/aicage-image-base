d1596e4ba025bee455cb9be302316e84a70d5a23
Stefan Kuhn <stefan.kuhn.zurich@gmail.com>
2026-08-13 02:26:34 +0200

    Run distro pre and post installers from Dockerfile

--- scripts/os-installers/distro/redhat/install.sh
+++ scripts/os-installers/distro/redhat/install.sh
@@ -7,10 +7,6 @@ script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 install_dir="${script_dir}/install"
 
-"${script_dir}/pre-install.sh"
-
 for install_script in "${install_dir}"/*.sh; do
   "${install_script}"
 done
-
-"${script_dir}/post-install.sh"

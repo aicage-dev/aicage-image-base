86a89aaaf6ac0bd3e0e4285ac425d98cc847ea88
Stefan Kuhn <stefan.kuhn.zurich@gmail.com>
2026-08-13 02:16:19 +0200

    Split distro installer pre and post steps

--- scripts/os-installers/distro/alpine/install.sh
+++ scripts/os-installers/distro/alpine/install.sh
@@ -6,6 +6,10 @@ apk add --no-cache "${AICAGE_PACKAGE_BASH_INSTALL:-bash}"
 script_dir="$(CDPATH='' cd -- "$(dirname "$0")" && pwd)"
 install_dir="${script_dir}/install"
 
+"${script_dir}/pre-install.sh"
+
 for install_script in "${install_dir}"/*.sh; do
   "${install_script}"
 done
+
+"${script_dir}/post-install.sh"

19fdafe6713e7f1e227cbf400639b6545a810fc6
Stefan Kuhn <stefan.kuhn.zurich@gmail.com>
2026-08-13 02:26:34 +0200

    Run distro pre and post installers from Dockerfile

--- scripts/os-installers/distro/arch/install.sh
+++ scripts/os-installers/distro/arch/install.sh
@@ -4,10 +4,6 @@ set -euo pipefail
 script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 install_dir="${script_dir}/install"
 
-"${script_dir}/pre-install.sh"
-
 for install_script in "${install_dir}"/*.sh; do
   "${install_script}"
 done
-
-"${script_dir}/post-install.sh"

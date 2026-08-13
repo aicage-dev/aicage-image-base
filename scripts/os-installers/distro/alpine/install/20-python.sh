9fd1c3cb24df0426ac95ffbd3e569a33c88ed84a
Stefan Kuhn <stefan.kuhn.zurich@gmail.com>
2026-08-13 02:36:18 +0200

    Enforce duplicated installer equality

--- scripts/os-installers/distro/alpine/install/20-python.sh
+++ scripts/os-installers/distro/alpine/install/20-python.sh
@@ -1,12 +1,6 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-apk add --no-cache \
-  "${AICAGE_PACKAGE_PY3_PIP_INSTALL:-py3-pip}" \
-  "${AICAGE_PACKAGE_PYTHON3_INSTALL:-python3}" \
-  "${AICAGE_PACKAGE_PY3_VIRTUALENV_INSTALL:-py3-virtualenv}" \
-  "${AICAGE_PACKAGE_PYTHON3_DEV_INSTALL:-python3-dev}"
-
 : "${PIPX_HOME:?PIPX_HOME is required}"
 : "${PIPX_BIN_DIR:?PIPX_BIN_DIR is required}"
 
